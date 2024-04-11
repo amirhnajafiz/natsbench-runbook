20acf0db279baa2afea4467ca902c4c98f82efe2
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-11 20:37:44 +0330

    init: exporter

--- internal/exporter/__init__.py
+++ internal/exporter/__init__.py
@@ -1,1 +1,11 @@
 # exporter is used to create output files
+import os.path
+
+
+OUTPUTDIR = "tmp"
+
+"""exists method checks for tmp dir, and makes sure that
+the OUTPUTDIR exists.
+"""
+def exists() -> bool:
+    return os.path.isdir(OUTPUTDIR)
