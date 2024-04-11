805fae6516e6e146fe6f5253f5a063847b9ffa94
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-11 20:21:02 +0330

    init: project

---
+++ main.py
@@ -0,0 +1,7 @@
+# todo: create a tmp directory
+# todo: parse cmd.json file
+# todo: create a dir for each command in tmp
+# todo: execute each command base on its count
+# todo: save the results into files (csv metrics and cmd output)
+# todo: create a parser to read these csv's and calculate the average in each file + a dataset
+# todo: perform Hypothesis test on the results for final output
