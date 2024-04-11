5e85df64fd944363e3d2bba648bb23b700698e92
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-11 20:53:44 +0330

    init: exec module

---
+++ internal/cmd/exec.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+"""run command executes a nats bench command.
+params:
+    - command: string
+
+returns:
+    - string
+"""
+def run(command: str) -> tuple[str, str]:
+    # split command to an array
+    subcommands = command.split(" ")
+    
+    # execute the command using subprocess
+    out = subprocess.run(["nats", "bench", subcommands], stdout=subprocess.PIPE, text=True)
+    if out.returncode != 0:
+        return out.stderr, "failed to execute give command"
+    
+    return out.stdout, ""
+    
