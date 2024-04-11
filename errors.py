7ea2501432330e277d6421864fbfb35b97e2340d
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-11 21:10:01 +0330

    init: errors.py

---
+++ errors.py
@@ -0,0 +1,3 @@
+# a map of errors and proper messages
+
+ERR_CMDFILE = "a cmd.json file is missing"
--- main.py
+++ main.py
@@ -5,23 +5,24 @@
 # todo: save the results into files (csv metrics and cmd output)
 # todo: create a parser to read these csv's and calculate the average in each file + a dataset
 # todo: perform Hypothesis test on the results for final output
-
 from system.error import panic
 from internal.config import exists
 from internal.config import load
 from internal.cmd.cmd import run
 
+import errors as es
+
 
 def main():
     if not exists():
-        panic("somethig")
+        panic(es.ERR_CMDFILE, 1)
     
     cfg = load()
     
     for item in cfg:
-        for index in range(0, int(cfg["count"])):
+        for index in range(0, int(item["count"])):
             out = run(cfg["command"])
-            print(out)
+            print(index, out)
 
 
 if __name__ == "__main__":
