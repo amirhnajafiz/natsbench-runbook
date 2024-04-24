5bef955344e8e8186d43bc9d397db8e98e58188f
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-24 10:46:26 +0330

    init: exporter methods

--- exporter/main.go
+++ exporter/main.go
@@ -1,5 +1,15 @@
 package main
 
+// serverCSV loads the csv file of the given project.
+func serveCSV() {
+
+}
+
+// listDirectories returns the list of tests
+func listDirectories() {
+
+}
+
 func main() {
 
 }
