a9f18f434bcb330e884893939805a35f2f159dd3
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-24 11:12:56 +0330

    add: exporter meta

---
+++ internal/exporter/meta.py
@@ -0,0 +1,16 @@
+from writer import export
+
+import json
+
+
+"""create metafile gets the command data in dictionary and creates a info.json
+in the destination.
+
+params:
+    - context: dictionary
+    - location: string
+"""
+def create_metafile(context: dict, location: str):
+    data = json.dumps(context)
+    path = f'{location}/info.json'
+    export(data, path)
