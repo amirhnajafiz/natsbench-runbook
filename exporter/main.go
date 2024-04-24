574c992c738c803df21128372735cee3da7b2026
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-24 11:30:30 +0330

    init: list directory method in exporter

--- exporter/main.go
+++ exporter/main.go
@@ -1,15 +1,58 @@
 package main
 
-// serverCSV loads the csv file of the given project.
-func serveCSV() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
 
+const (
+	baseDir = "tmp/"
+)
+
+type FileMeta struct {
+	Date     time.Time `json:"date"`
+	Location string    `json:"location"`
+}
+
+// serverCSV loads the csv file of the given project.
+func serveCSV(location string) {
+	path := fmt.Sprintf("%s%s/dataset.csv", baseDir, location)
+
+	log.Println(path)
 }
 
 // listDirectories returns the list of tests
-func listDirectories() {
+func listDirectories() []string {
+	items := make([]string, 0)
 
+	entries, err := os.ReadDir(baseDir)
+	if err != nil {
+		log.Println(err)
+
+		return items
+	}
+
+	for _, e := range entries {
+		data, err := os.ReadFile(fmt.Sprintf("%s%s/info.json", baseDir, e.Name()))
+		if err != nil {
+			log.Println(err)
+
+			continue
+		}
+
+		items = append(items, string(data))
+	}
+
+	return items
 }
 
 func main() {
+	list := listDirectories()
 
+	for _, item := range list {
+		fmt.Println(item)
+		fmt.Println()
+	}
 }
