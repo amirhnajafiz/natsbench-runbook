e910a73d65356c88a4dd836f0e3fe1e3d86e7c53
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-27 10:25:57 +0330

    add: config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,14 @@
 package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func Load() map[string]interface{} {
+	instance := make(map[string]interface{})
+
+	instance["port"], _ = strconv.Atoi(os.Getenv("HTTP_PORT"))
+
+	return instance
+}
