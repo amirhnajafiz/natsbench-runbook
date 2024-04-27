10dede7a22d74a92a7c8e68d20bac0a337b31f91
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-27 10:27:50 +0330

    init: cli

--- internal/nats/cli.go
+++ internal/nats/cli.go
@@ -1,1 +1,25 @@
 package nats
+
+type CLI interface {
+	List() []string
+	Get(name string) string
+	Select(name string)
+}
+
+type cli struct{}
+
+func New() CLI {
+	return &cli{}
+}
+
+func (c cli) List() []string {
+	return nil
+}
+
+func (c cli) Get(name string) string {
+	return ""
+}
+
+func (c cli) Select(name string) {
+
+}
