bad0a4e3b1a85f0ba468cce681c1224e090b516a
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-28 09:01:34 +0330

    feat: handler list ctx

--- internal/handler/context.go
+++ internal/handler/context.go
@@ -1,9 +1,25 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func (h Handler) ListContext(w http.ResponseWriter, r *http.Request) {
+	list := h.NatsCli.List()
 
+	bytes, err := json.Marshal(list)
+	if err != nil {
+		log.Println(err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(bytes)
 }
 
 func (h Handler) GetContext(w http.ResponseWriter, r *http.Request) {
