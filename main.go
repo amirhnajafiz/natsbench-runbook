8ad4b6f0fde9f7965061a18c90cd3368618eab0a
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-28 08:59:50 +0330

    add: mux router endpoints

--- main.go
+++ main.go
@@ -5,25 +5,39 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/amirhnajafiz/natsbench-runbook/internal/config"
+	"github.com/amirhnajafiz/natsbench-runbook/internal/handler"
+	"github.com/amirhnajafiz/natsbench-runbook/internal/nats"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	// load configs
+	cfg := config.Load()
+
 	// create a new mux router
 	router := mux.NewRouter()
 
+	// create a new handler
+	h := handler.Handler{
+		NatsCli: nats.New(),
+	}
+
 	// register exporter endpoints
-	//router.HandleFunc("/healthz", h.Health).Methods(http.MethodGet)
-	//router.HandleFunc("/benchmarks", h.ListDirectories).Methods(http.MethodGet)
-	//router.HandleFunc("/benchmarks/{benchmark}", h.ServeCSV).Methods(http.MethodGet)
+	router.HandleFunc("/healthz", h.Health).Methods(http.MethodGet)
+	router.HandleFunc("/context", h.ListContext).Methods(http.MethodGet)
+	router.HandleFunc("/context/{context}", h.GetContext).Methods(http.MethodGet)
+	router.HandleFunc("/context/{context}", h.SelectContext).Methods(http.MethodPost)
+	router.HandleFunc("/", h.Runbook).Methods(http.MethodPost)
 
 	// create a new server
 	srv := &http.Server{
 		Handler: router,
-		Addr:    fmt.Sprintf("127.0.0.1:%d", 8080),
+		Addr:    fmt.Sprintf("127.0.0.1:%d", cfg["port"].(int)),
 	}
 
-	log.Printf("exporter started on %d ...\n", 8080)
+	log.Printf("server started on %d ...\n", cfg["port"].(int))
 
 	// start the http server
 	if err := srv.ListenAndServe(); err != nil {
