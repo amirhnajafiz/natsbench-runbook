9fbcbcec40c19ae28ebf0d13f080bfdbb0bde96f
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-29 08:44:00 +0330

    update: model of cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -6,24 +6,24 @@ import (
 )
 
 type Cache struct {
-	storage map[string]item
+	storage map[string]*item
 }
 
 func New() Cache {
 	return Cache{
-		storage: make(map[string]item),
+		storage: make(map[string]*item),
 	}
 }
 
 func (c Cache) Put(name, command string) {
-	c.storage[name] = item{
+	c.storage[name] = &item{
 		CreatedAt: time.Now(),
 		Command:   command,
 	}
 }
 
 func (c Cache) Del(name string) {
-	delete(c.storage, name)
+	c.storage[name].FinishedAt = time.Now()
 }
 
 func (c Cache) List() []byte {
--- internal/cache/model.go
+++ internal/cache/model.go
@@ -3,6 +3,7 @@ package cache
 import "time"
 
 type item struct {
-	CreatedAt time.Time `json:"created_at"`
-	Command   string    `json:"command"`
+	CreatedAt  time.Time `json:"created_at"`
+	FinishedAt time.Time `json:"finished_at"`
+	Command    string    `json:"command"`
 }
